7ddaf4a212349c8abbc7bd1af15b397d887a3a54
Ahiwe09 <ahiwe12345@gmail.com>
2023-09-17 06:47:33 -0700

    header_file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MY_PRINTF_H
+#define MY_PRINTF_H
+
+/**
+* main.h - This file contain the prototype of all the function
+* we will be using in this project
+* authors:Micheal oko Nwandinobi Ahiwe.
+*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
