83d1be9b2ef38d4b4f85fea36004c913a81f2d9b
Ahiwe09 <ahiwe12345@gmail.com>
2023-09-19 06:01:21 -0700

    1-printf project

--- play_char.c
+++ play_char.c
@@ -3,8 +3,7 @@
  * play_char - This function print charater on
  * the standerd output
  * @lst: This is a list of the variable part
- * of the variadic function
- *
+ * of the variadic function.
  * Return: This function returns an int variable
  */
 int play_char(va_list lst)
